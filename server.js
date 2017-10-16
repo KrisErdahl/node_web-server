dc45093e81268669608da86f782ae7f6eaa689fc
KrisErdahl <>
2017-10-16 13:18:43 -0500

    initial commit

---
+++ server.js
@@ -0,0 +1,65 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+//need to call the method, details below
+const app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+	const now = new Date().toString();
+	const log = `${now}: ${req.method} ${req.url}`;
+	console.log(log);
+	fs.appendFile('server.log', log + '\n', err => {
+		if (err) {
+			console.log('Unable to append to server.log.');
+		}
+	});
+	next();
+});
+
+//Activate for maintenance mode
+// app.use((req, res, next) => {
+// 	res.render('maintenance.hbs');
+// });
+
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+	return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', text => {
+	return text.toUpperCase();
+});
+
+//http event handlers
+app.get('/', (req, res) => {
+	res.render('home.hbs', {
+		pageTitle: 'Home Page',
+		welcomeMessage: "We're glad you are here! Welcome!"
+	});
+	// res.send('<h1>Hello Express!</h1>');
+	// res.send({
+	// 	name: 'Kris',
+	// 	likes: ['Family', 'Dogs']
+	// });
+});
+
+app.get('/about', (req, res) => {
+	res.render('about.hbs', {
+		pageTitle: 'About Page'
+	});
+});
+
+app.get('/bad', (req, res) => {
+	res.send({
+		errorMessage: 'Unable to handle this request'
+	});
+});
+
+app.listen(3500, () => {
+	console.log('Server is up on port 3500');
+});
